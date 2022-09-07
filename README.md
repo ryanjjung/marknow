82ac0b3df40315a4db069ae90fa3a856b3cd2c06
ryanjjung <ryanjjung@gmail.com>
2022-09-06 20:39:05 -0600

    Get the basics started

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # marknow
+
 A server of browsable Markdown
+
+## Usage
+
+    marknow -d /path/to/markdown/files/
---
+++ marknow/monitor.py
@@ -0,0 +1,16 @@
+from flask import Blueprint
+from lib import HTMLResponse
+
+HTML='''<!DOCTYPE html>
+<html>
+<head><title>I'm Alive!</title></head>
+<body><h1>I'm Alive!</h1></body>
+</html>
+'''
+
+bp = Blueprint('monitor', __name__, url_prefix='/_')
+
+@bp.route('/ping', methods=['GET'])
+def ping():
+    '''Return a successful message to show the service is alive'''
+    return HTMLResponse(status=200, response=HTML)
